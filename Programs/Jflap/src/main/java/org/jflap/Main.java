60a7a8324fbe6931c6b97086a10052775d763077
Patman1O1 <patrickthomas101x@gmail.com>
2025-09-20 15:20:04 -0500

    Made JFLAP runnable and added directories to save work to

--- Programs/Jflap/src/main/java/org/jflap/Main.java
+++ Programs/Jflap/src/main/java/org/jflap/Main.java
@@ -1,10 +1,40 @@
 package org.jflap;
 
+import java.io.File;
+import java.io.IOException;
 
+/*
+* Frequently Used Characters
+* ε
+* Σ
+* */
 
 public class Main {
-    public static void main(String[] args) {
+    public static final File JFLAP_FILE = new File("C:\\Users\\patri\\OneDrive\\Coding\\Java\\Programs\\Jflap\\",
+                                             "JFLAP7.1.jar");
 
+    public static void runJflap() {
+        try {
+            // Start JFLAP
+            Process jflapProcess = new ProcessBuilder("java", "-jar", JFLAP_FILE.getAbsolutePath()).inheritIO().start();
 
+            // 🔹 If your program ends, destroy JFLAP
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                if (jflapProcess.isAlive()) {
+                    jflapProcess.destroy();
+                }
+            }));
+
+            // 🔹 If JFLAP ends, exit your program
+            int exitCode = jflapProcess.waitFor();
+            System.out.println("JFLAP exited with code: " + exitCode);
+            System.exit(exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void main(String[] args) { runJflap(); }
+
 }
