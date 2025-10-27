e709fbc15c6f9c777a977e2cd7dcd7a6e2081c74
Patman1O1 <patrickthomas101x@gmail.com>
2025-10-27 08:41:45 -0500

    Initial Commit

---
+++ Programs/Grade_Calculator/src/test/java/CourseTests.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CourseTests {
+    /* --------------------------------------------------Fields------------------------------------------------------ */
+
+    /* --------------------------------------------------SetUp------------------------------------------------------- */
+    //@BeforeEach
+    //protected void setUp() { kenoLogic = new KenoLogic(); }
+
+    /* ---------------------------------------------Constructor Tests------------------------------------------------ */
+
+    /* -----------------------------------------------Setter Tests--------------------------------------------------- */
+
+    /* -----------------------------------------------Getter Tests--------------------------------------------------- */
+
+    /* -----------------------------------------------Method Tests--------------------------------------------------- */
+
+}
