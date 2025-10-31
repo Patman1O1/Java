39451e1c0707061079430b819da5bb44c16ea3c0
Patman1O1 <patrickthomas101x@gmail.com>
2025-10-31 14:07:16 -0500

    Added Menu Bar

--- Programs/Grade_Calculator/src/main/java/com/gradecalc/gui/EditCourseController.java
+++ Programs/Grade_Calculator/src/main/java/com/gradecalc/gui/EditCourseController.java
@@ -1,8 +1,13 @@
 package com.gradecalc.gui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -15,11 +20,41 @@ public class EditCourseController {
     @FXML
     private BorderPane sceneRoot;
 
+    @FXML
+    protected MenuBar menuBar;
+
+    @FXML
+    protected MenuItem newCourseItem;
+
+    @FXML
+    protected MenuItem exitItem;
+
     /* -----------------------------------------------Constructors--------------------------------------------------- */
     public EditCourseController() {}
 
     /* -------------------------------------------------Methods------------------------------------------------------ */
     @FXML
     public void initialize(URL location, ResourceBundle resources) {}
+
+    @FXML
+    public void showNewCourseScene() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/NewCourseFXML.fxml"));
+            Parent newCourseRoot = fxmlLoader.load();
+
+            newCourseRoot.getStylesheets().add("/css/styles.css");
+
+            this.sceneRoot.getScene().setRoot(newCourseRoot);
+
+            Stage stage = (Stage) newCourseRoot.getScene().getWindow();
+            stage.sizeToScene();
+            stage.centerOnScreen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void exitApp() { Platform.exit(); }
 
 }
--- Programs/Grade_Calculator/src/main/java/com/gradecalc/gui/NewCourseController.java
+++ Programs/Grade_Calculator/src/main/java/com/gradecalc/gui/NewCourseController.java
@@ -1,7 +1,13 @@
 package com.gradecalc.gui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -14,11 +20,41 @@ public class NewCourseController {
     @FXML
     protected BorderPane sceneRoot;
 
+    @FXML
+    protected MenuBar menuBar;
+
+    @FXML
+    protected MenuItem editCourseItem;
+
+    @FXML
+    protected MenuItem exitItem;
+
     /* -----------------------------------------------Constructors--------------------------------------------------- */
     public NewCourseController() {}
 
     /* -------------------------------------------------Methods------------------------------------------------------ */
     @FXML
     public void initialize(URL location, ResourceBundle resources) {}
+
+    @FXML
+    public void showEditCourseScene() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/EditCourseFXML.fxml"));
+            Parent editCourseRoot = fxmlLoader.load();
+
+            editCourseRoot.getStylesheets().add("/css/styles.css");
+
+            this.sceneRoot.getScene().setRoot(editCourseRoot);
+
+            Stage stage = (Stage) editCourseRoot.getScene().getWindow();
+            stage.sizeToScene();
+            stage.centerOnScreen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void exitApp() { Platform.exit(); }
 
 }
